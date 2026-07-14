54bc0945361144baca00f2590412d4ba17bde652
kavya <kavya.chawla23@st.niituniversity.in>
2026-07-14 19:53:36 +0530

    Add MongoDB database connection

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +15,7 @@ app.get("/", (req, res) => {
   res.send("DataLens API is running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`DataLens backend running on port ${PORT}`);
