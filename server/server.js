282b619b172dde9ca6a1f2f20355a96578820e30
kavya <kavya.chawla23@st.niituniversity.in>
2026-07-14 13:06:40 +0530

    Initial MERN project setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("DataLens API is running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`DataLens backend running on port ${PORT}`);
+});
